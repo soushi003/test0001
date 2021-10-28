2ad43865a4a202c3e52cdf82dba6fcf0e01f652c
soushi003 <soushi.h6@icloud.com>
2021-10-28 10:35:19 +0900

    hs1

---
+++ OculusQuest2 Test/Assets/pickBox.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickBox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    //è‚ğ“ü‚êAo‚µ‚½‚Æ‚«‚ÉèŒ³‚Éo‚·
+    void Update()
+    {
+        
+    }
+}
