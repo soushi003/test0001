91fa03f697838181ddc7d1c3a18d6c74473d9991
soushi003 <soushi.h6@icloud.com>
2021-10-29 15:10:04 +0900

    s

---
+++ OculusQuest2 Test/Assets/hashitani/Mystery Safe.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mystery_Safe : MonoBehaviour
+{
+    fixed int open_count[4];
+    //ƒ‰ƒ“ƒ_ƒ€‚É10‰ñ“ü‚ê‚é
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
