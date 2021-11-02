196bf00f81f2034f4932ec9b48dc1cb805a94171
soushi003 <soushi.h6@icloud.com>
2021-11-02 16:13:19 +0900

    a

--- OculusQuest2 Test/Assets/hashitani/Mystery Safe.cs
+++ OculusQuest2 Test/Assets/hashitani/Mystery Safe.cs
@@ -4,17 +4,49 @@ using UnityEngine;
 
 public class Mystery_Safe : MonoBehaviour
 {
-    fixed int open_count[4];
-    //ランダムに10回入れる
+    int upC, rC, downC, lC = 0;
+    int upD, rD, downD, lD = 0;
+    int putD = 0;
+    int setD = 0;
+    //触れている
+    bool hand = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        upC = 3;
+        rC = 3;
+        downC = 2;
+        lD = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (hand)
+        {
+            //回転に応じてputDを変更
+        }
+        else if (!hand&&setD!=putD)
+        {
+            setD = putD;
+            switch (setD)
+            {
+                case 0:upD++; break;
+                case 1:rD++; break;
+                case 2:downD++; break;
+                case 3:lD++; break;
+            }
+            if (upD == upC && rD == rC && downD == downC && lD == lC)
+            {
+                //解除
+            }
+            else if(upD > upC || rD > rC || downD > downC || lD > lC)
+            {
+                //リセット
+                upD = rD = downD = lD = 0;
+                setD = 0;
+                putD = 0;
+            }
+        }
     }
 }
---
+++ OculusQuest2 Test/Assets/hashitani/flesh.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flesh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //“–‚½‚è”»’è‚ğg‚Á‚Ä–Ú‚ğ‚Í‚ß‚Ş
+        //è‚ğ—£‚·‚Æ–Ú‚ª‚Í‚Ü‚é,æ‚ê‚È‚­‚È‚é
+        //”à‚ªŠJù‚³‚ê‚é
+    }
+}
